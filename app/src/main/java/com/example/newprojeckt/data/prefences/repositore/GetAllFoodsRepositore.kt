bb35010b96e3107de27dc7c4afb305d097efda92
Macbook <macbook@macbooks-macbook-pro.local>
2023-12-03 15:09:58 +0600

    Initial commit

--- app/src/main/java/com/example/newprojeckt/data/prefences/repositore/GetAllFoodsRepositore.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newprojeckt.data.prefences.repositore
-
-import com.example.newprojeckt.data.prefences.model.FoodModel
-
-interface GetAllFoodsRepositore {
-    fun getAllFoods(): List<FoodModel>
-}
--- app/src/main/java/com/example/newprojeckt/data/prefences/repositore/GetAllFoodsRipositoreImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.example.newprojeckt.data.prefences.repositore
-
-import com.example.newprojeckt.data.prefences.face_data.foodFaceDatas
-import com.example.newprojeckt.data.prefences.model.FoodModel
-
-class GetAllFoodsRipositoreImpl : GetAllFoodsRepositore {
-    override fun getAllFoods(): List<FoodModel> = foodFaceDatas()
-
-}
